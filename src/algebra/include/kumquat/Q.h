2441d78053a39c7a6e74f20b60a22504698523a8
MARIA Clement <clement.maria@inria.fr>
2023-12-06 17:52:01 +0100

    Field of rationals Q

--- src/algebra/include/kumquat/Q.h
+++ src/algebra/include/kumquat/Q.h
@@ -10,8 +10,8 @@
  *      - YYYY/MM Author: Description of the modification
  */
 
-#ifndef KUMQUAT_Q_MOD_Z_H_ 
-#define KUMQUAT_Q_MOD_Z_H_
+#ifndef KUMQUAT_Q_H_ 
+#define KUMQUAT_Q_H_
 
 #include <string>
 #include <vector>
@@ -22,40 +22,32 @@
 
 namespace kumquat {
 
-/** \class Q_mod_Z Q_mod_Z.h kumquat/Q_mod_Z.h 
-  * \brief Abelian group \f$\mathbb{Q}/\mathbb{Z}\f$, or its subgroup 
-  * \f$\mathbb{Q}_{(p)}/\mathbb{Z}\f$ for a prime p.
+/** \class Q Q.h kumquat/Q.h 
+  * \brief Field of rational numbers \f$\mathbb{Q}\f$.
   *
-  * The structure is an Abelian group for +.
+  * The structure is a field \f$(\mathbb{Q},0,+,\times,1)\f$.
   * 
   * Elements are represented by pairs of multiprecision non-negative integers (x,y), 
-  * repesenting fractions \f$x/y\f$, with \f$0 \geq x < y\f$ and gcd(x,y) = 1.
+  * representing fractions \f$x/y\f$, with \f$0 < y\f$ and gcd(x,y) = 1.
   *
-  * \implements AbelianGroup
+  * \implements Ring
   * 
   * template IntegerNumber implements IntegerNumber
   */
 template<typename IntegerNumber>
-class Q_mod_Z {
+class Q {
 public:
   static const bool abelian_group = true;
-  static const bool pseudo_ring = false;//multiplication undefined a/b*c/d != a/b*(c+d)/d
-  static const bool ring = false;
-  static const bool principal_ideal_domain = false;
-  static const bool field = false;
+  static const bool pseudo_ring = true;//multiplication undefined a/b*c/d != a/b*(c+d)/d
+  static const bool ring = true;
+  static const bool principal_ideal_domain = true;
+  static const bool field = true;//the multiplicative inverse always exists, except for 0
 
-/** \brief Initialisation of the entire group \f$\mathbb{Q}/\mathbb{Z}\f$.
+/** \brief Initialization of the entire group \f$\mathbb{Q}/\mathbb{Z}\f$.
   * 
   * p_ set to -1 indicates that we construct the entire group.
   * */
-  Q_mod_Z() : p_(-1) {}
-/** \brief Initialisation of a subgroup \f$\mathbb{Q}_{(p)}/\mathbb{Z}\f$ of the 
-  * group \f$\mathbb{Q}/\mathbb{Z}\f$.
-  * 
-  * p_ must be a prime number. Elements are all of the form \f$u / p^k\f$, 
-  * \f$ k>0 \f$ and \f$ 0 \leq u < p^k \f$ and gcd(u,p) = 1.
-  * */
-  Q_mod_Z(int p) : p_(p) {}
+  Q() {}
 
 /** \name Methods for Abelian groups. Implements AbelianGroup.
  * @{ */
@@ -70,7 +62,9 @@ public:
 /** \brief The type of elements of the group.
   * 
   * Elements are represented by a pair of multiprecision integers \f$(x,y)\f$, 
-  * representing the fraction \f$x/y\f$, with \f$0 \leq x < y\f$ and \f$gcd(x,y) = 1\f$. 
+  * representing the fraction \f$x/y\f$, with \f$0 < y\f$ and \f$gcd(x,y) = 1\f$. 
+  * 
+  * The additive inverse is 0/1.
   * 
   * Must be copiable. 
   * */
@@ -97,10 +91,6 @@ public:
  * the group.
  * */
   void times_equal(Element & a, Integer z) { 
-    if(z<0) {
-      z = -1 * z;
-      a = additive_inverse(a);//inverse
-    }
     a.first *= z; 
     normalize(a); 
   }
@@ -108,10 +98,6 @@ public:
  * group.
  * */
   Element times(Element a, Integer z) { 
-    if(z<0) {
-      z = -1 * z;
-      a = additive_inverse(a);//inverse
-    }
     a.first *= z; 
     normalize(a); 
     return a;
@@ -119,7 +105,7 @@ public:
 /** \brief Return the additive inverse (-a) of an element a. */
   Element additive_inverse(Element a) { 
     if(a.first == 0) return additive_identity();
-    return Element(a.second - a.first, a.second); //1-a
+    return times(a,-1);
   }
 /** \brief Return the order of the group. 
  * 
@@ -134,8 +120,7 @@ public:
  * */
   Integer order(Element a) { 
     if(a.first == 0) { return 0; }
-    auto order_mp = kumquat::gcd_complement(a.second, a.first);
-    return order_mp; 
+    return -1;
   }
 /** \brief Return the rank of the group, i.e., the minimal number of generators. 
  * 
@@ -153,7 +138,7 @@ public:
  * 
  * This input integer is unused as all integers are converted to 0.
  */
-  Element element([[maybe_unused]] Integer z) { return Element(0,1); }
+  Element element(Integer z) { return Element(z,1); }
 /** \brief Return true iff the input a is equal to the additive identity 0.*/
   bool trivial(Element a) { return a.first == 0; }
   bool trivial(Integer z) { return z == 0; }
@@ -162,18 +147,18 @@ public:
  * @{ */
 
 /** Set a <- (a*b). */
-    // void times_equal(Element & a, Element b) {
-    //   a.first = a.first * b.first;
-    //   a.second = a.second * b.second;
-    //   normalize(a);
-    // }
-/** Return a*b.*/
-    // Element times(Element a, Element b) {
-    //   a.first = a.first * b.first;
-    //   a.second = a.second * b.second;
-    //   normalize(a);
-    //   return a;
-    // }
+  void times_equal(Element & a, Element b) {
+    a.first = a.first * b.first;
+    a.second = a.second * b.second;
+    normalize(a);
+  }
+// /** Return a*b.*/
+  Element times(Element a, Element b) {
+    a.first = a.first * b.first;
+    a.second = a.second * b.second;
+    normalize(a);
+    return a;
+  }
 /** Set a <- a^p. */
     void pow_equal(Element & a, Integer p) {
       a.first = kumquat::pow(a.first,p);
@@ -186,11 +171,23 @@ public:
       return a;
     }
 /* @} */  // end pseudo ring methods
-
-
-/** \name Methods specific to Q / Z and Q_{p} / Z
+/** \name Methods for rings. Implements Ring.
  * @{ */
-/** \brief Construct the Element \f$x/y \mod \mathbb{Z}\f$ from two integers 
+/** Return the multiplicative identity 1.*/
+    Element multiplicative_identity() {
+      return Element(1,1);
+    }
+/** Return the multiplicative inverse of a Element a if it exists, and return the additive identity 0 otherwise.*/
+    Element multiplicative_inverse(Element a) {
+      if(trivial(a)) return additive_identity();
+      return Element(a.second,a.first);
+    }
+/* @} */  // end ring methods
+
+
+/** \name Methods specific to Q
+ * @{ */
+/** \brief Construct the Element \f$x/y\f$ from two integers 
  * /f$x/f$ and /f$y/f$.
  * */ 
   Element element(Integer x, Integer y) { 
@@ -198,39 +195,15 @@ public:
     normalize(z);
     return z; 
   }
-/** \brief Return the Integer p in case we represent the subgroup 
- * \f$\mathbb{Q}_{(p)}/\mathbb{Z}\f$ of \f$\mathbb{Q}/\mathbb{Z}\f$.
- * 
- * Must return a prime number p, or -1 in case we represent the entire group 
- * \f$\mathbb{Q}/\mathbb{Z}\f$.
- * */
-  Integer p() { return p_; }
-/** \brief Normnalize a fraction u/v into x/y such that:
- * u/v = x/y modulo Z, and 
- * gcd(x,y) = 1, and 
- * 0 <= x < y.
+/** \brief Normalize a fraction u/v into x/y such that:
+ * u/v = x/y, and gcd(x,y) = 1, and 0 < y.
  */ 
   void normalize(Element & a) {
-    normalize_mod_Z(a);
     normalize_fraction(a);
   }
 /* @} */  // end methods specific to Q / Z
 
-// /** \brief Write a fraction a/b, with gcd(a,b)=1, as a product a * 1/b, with a and Integer and 1/b an Element.*/
-//   std::pair<Integer,Element> externalize(Element & a) {
-//     normalize(a);
-//     return std::make_pair(a.first, Element((Integer)1, a.second));
-//   }
-
-
 private:
-  /* Normalize the element a modulo \f$\mathbb{Z}\f$. 
-   * 
-   * Note that we do not enforce that \f$\gcd(a.first,a.second) == 1\f$.
-   */
-  void normalize_mod_Z(Element & a) {
-    if(a.first >= a.second) { a.first = (a.first) % (a.second); }
-  }
   /* Normalize the fraction such that gcd(numerator,denominator) = 1. 
    */
   void normalize_fraction(Element & a) {
@@ -240,14 +213,8 @@ private:
       a.first /= gcd;
       a.second /= gcd;
     }
+    if(a.second < 0) { a.first *= -1; a.second *= -1; }
   }
-  /** A prime number p in case the structure represents the subgroup 
-   * \f$\mathbb{Q}_{(p)}/\mathbb{Z}\f$ of \f$\mathbb{Q}/\mathbb{Z}\f$. 
-   * 
-   * Must be a prime number, or -1.
-   * */
-  Integer p_;
-
 };
 
 /** \brief Write an Element of Q_mod_Z into a stream, as an unnormalized fraction 
@@ -269,7 +236,7 @@ std::ostream & operator<<( std::ostream & os,
   return os;
 }
 
-typedef Q_mod_Z<boost::multiprecision::mpz_int> Q_mod_Z_mp;
+typedef Q<boost::multiprecision::mpz_int> Q_mp;
 
 }  //namespace kumquat
 
