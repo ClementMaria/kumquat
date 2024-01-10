7d9ba96ba38e640d4c0d983d351381e1087c5cc2
Clement Maria <clement.maria@inria.fr>
2024-01-10 11:39:30 +0100

    example markov process

--- src/random_process/example/simple_markov_decision_example.cpp
+++ src/random_process/example/simple_markov_decision_example.cpp
@@ -14,22 +14,89 @@
 
 using namespace kumquat;
 
-int main(int argc, char * argv[]) {
+typedef Markov_decision::Proba_float P_float;
 
-  std::vector<size_t> num_states = {10,3,10,5,10,8,10};
+int main() {
+  //num states per layer
+  std::vector<size_t> num_states = {20,39,50,150,400,80,10};
   Markov_decision mdp(num_states);
 
-  size_t sum=0;
-  for(auto n : num_states) { sum += (n-1); }
-  //amplify the sum of indices in the path, min is 0, max is 9+2+9+... = sum
-  //return sum_state/sum - 1/2 in  [-0.5;0.5]
-  Markov_decision::Proba_float rew_alpha = [&](std::vector<size_t> samp) {
-    size_t sum_state=0; for(auto n : samp) { sum_state += (n-1); }
-    return ((Proba_float)(sum_state)/(Proba_float)(sum) - (Proba_float)(0.5));
+  // size_t sum=0;
+  // for(auto n : num_states) { sum += (n-1); }
+  // //amplify the sum of indices in the path, min is 0, max is sum
+  // //return 0.01*[sum_state/sum - 1/2] in  [-0.005;0.005]
+  // auto rew_sum = [&](std::vector<size_t> &samp)->P_float
+  // {
+  //   size_t sum_state=0; 
+  //   for(auto n : samp) { sum_state += n; }
+  //   // return 0;
+  //   return 0.01*((P_float)(sum_state)/(P_float)(sum) - (P_float)(0.5));
+  // };
+
+  //non-linear reward based on a polynomial on the labels of nodes in the Markov process
+  auto rew_poly = [&](std::vector<size_t> &samp)->P_float
+  {
+    P_float s0 = (P_float)samp[0];
+    P_float s1 = (P_float)samp[1];
+    P_float s2 = (P_float)samp[2];
+    P_float s3 = (P_float)samp[3];
+    P_float s4 = (P_float)samp[4];
+    P_float s5 = (P_float)samp[5];
+    P_float s6 = (P_float)samp[6];
+
+    auto p_x = s0*s1*s5 - s0*s3*s4 + s0*s5*s6 - s2*s5*s1 + s3*s4*s2;
+     
+    return (p_x / (P_float)(150*400*40))-0.1;
   };
 
-  size_t num_iteration = 1000;
-  mdp.train(num_iteration, rew_alpha);
+  //evaluate randomly the polynomial reward to later normalize it
+  std::cout << "Out of 1000 rewards: ";
+  P_float minr = 0;
+  P_float maxr = 0; 
+  for(size_t k=0; k<1000; ++k) {
+    auto samp = mdp.sample();
+    auto tmpr = rew_poly(samp);
+    if(tmpr > maxr) { maxr = tmpr; }
+    if(tmpr < minr) { minr = tmpr; }
+    // std::cout << tmpr << " ";
+  }
+  std::cout << "\n" << "Min reward = " << minr << "  Max reward = " << maxr << "\n";
+
+  //normalized polynomial reward
+  auto rew_poly_balanced = [&](std::vector<size_t> &samp)->P_float
+  {
+    auto rew = rew_poly(samp);
+    rew -= (maxr+minr)/2.;
+    rew *= 0.01 * (1./(maxr-minr));
+    return rew;
+  };
+
+  //train until possibly stuck in a state
+  size_t num_iteration = 30000;//generally converges
+  auto max_samp = mdp.train(num_iteration, rew_poly_balanced);
+
+  //sample the state and brute force improve it a few times
+  std::vector<size_t> curr_samp;
+  auto imp_samp = max_samp;
+  do {
+    curr_samp = imp_samp;
+    imp_samp = mdp.local_improve(curr_samp, rew_poly_balanced);
+  } while(curr_samp != imp_samp);
+
+  // auto samp = mdp.sample();
+  // auto imp_samp = mdp.local_improve(max_samp, rew_poly);
+
+  std::cout << "\n\n\n";
+  std::cout << "Sample          = " << mdp.to_string(max_samp) << "of rew == " << rew_poly(max_samp) <<  "\n";
+  std::cout << "Improved sample = " << mdp.to_string(imp_samp) << "of rew == " << rew_poly(imp_samp) <<  "\n";
+  std::cout << "\n";
+
+  // //start a new Markow decision process
+  // Markov_decision mdp_1(num_states);
+  // //and push the imp_sample first to give it a direction
+  // mdp_1.amplify(imp_samp, 2.*rew_poly_balanced(imp_samp));
+  // //train to try to do better
+  // mdp_1.train(num_iteration, rew_poly_balanced);
 
   return 0;
 }
--- src/random_process/include/kumquat/Markov_decision.h
+++ src/random_process/include/kumquat/Markov_decision.h
@@ -18,49 +18,83 @@
 
 namespace kumquat {
 
-/** A decision matrix for a Markov decision process. The process is a complete 
+/** A decision matrix for a Markov decision process. The process is 
+ * a complete 
  * multipartite graph V0 V1 ... Vt, represented by a set of transition matrices 
  * M0 ... M_t-1. 
  * 
- * Matrix Mi is a (Vi x Vi+1) dense matrix, representing the transition of local state at time i to time i+1. Precisely, Mi[p] is a row vector of length Vi+1 containing the probability distribution of transitioning to the nodes of Vi+1, knowing that we are in the p-th node of Vi. 
+ * Matrix Mi is a (Vi x Vi+1) dense matrix, representing the 
+ * transition of local state at time i to time i+1. Precisely, 
+ * Mi[s] is a row vector of length Vi+1 containing the probability 
+ * distribution of transitioning to the nodes of Vi+1, knowing that 
+ * we are in the s-th node of Vi. 
  * 
- * Mi[p][q] is the probability to jumping to q \in Vi+1, knowing that we are in p at time i.
+ * Mi[p][q] is the probability to jumping to q \in Vi+1, knowing 
+ * that we are in p at time i.
  * 
+ * One state of the universe is exactly a left to right path in the 
+ * multipartite graph.
+ * 
+ * A state is a left to right path in the multipartite graph (i.e., a sequence v0 v1 ... vt of nodes in V0, V1, ... Vt respectively). The set of all states in the universe.
  **/
+// template<typename NodeType>
 class Markov_decision {
-
+public:
+  /** A float type to represent probabilities.**/
   typedef R::Element Proba_float;
 
-  //balanced probabilities 
-  //num_states[i] contains the size of |Vi|, i=0...t
+  // typedef NodeType Node_type;
+
+  /** \brief Initialize the transition matrices with uniform 
+   * probabilities. 
+   *
+   * @param[in] num_states the vector of sizes of the sets of nodes 
+   *                       num_states[i] = |Vi| of the multipartite 
+   *                       graph.
+   **/
   Markov_decision(std::vector<size_t> num_states) 
-  : num_layers_(num_states.size()) 
   {
-    //idd starting point
-    start_distribution_(num_states[0], R_.element(1)/R_.element(num_states[0]));    
-    decision_mat_( num_layers_-1 ); 
+    size_t num_layers = num_states.size(); 
+    //idd distribution for starting point
+    start_distribution_.reserve(num_states[0]);
+    Proba_float unif = R_.element(1)/R_.element(num_states[0]);
+    for(size_t i=0; i<num_states[0]; ++i) {
+      start_distribution_.push_back(unif);
+    }
+    //vector containing the transitions matrices
+    // decision_mat_.reserve( num_layers-1 ); 
   
-    for(size_t t=0; t<num_layers_-1; ++t) {
+    for(size_t t=0; t<num_layers-1; ++t) {
       size_t num_rows = num_states[t];
       size_t num_cols = num_states[t+1];
-      decision_mat_[t] = Dense_matrix<R>(num_rows,num_cols,R_); 
-      Proba_t f = [&](size_t i, size_t j) { return 1/num_cols; };    
-      decision_mat_.[t].fill(f);
+      decision_mat_.emplace_back(num_rows,num_cols,R_); 
+      //fill the matrices with appropriately normalize uniform proba
+      auto f = [&]([[maybe_unused]] size_t i, [[maybe_unused]] size_t j)->Proba_float { return (Proba_float)(1)/(Proba_float)num_cols; };   
+      decision_mat_[t].fill(f);
     }
   }
 
-//return a sample using the decision process
+  /** \brief Return a state sampled from the distribution of the Markov process. 
+   * 
+   * Transverse the mutipartite graph V0 ... Vt from left to right, 
+   * and transition by sampling the starting and transition 
+   * matrices.
+   * 
+   * @param[out] a vector of integers, representing the label of a node in V0, V1 ... Vt.
+   * **/
   std::vector< size_t > sample() {
-    std::random_device rd;
+    std::random_device rd;//initialize random generator
     std::mt19937 gen(rd());
 
-    std::vector<size_t> samp(num_layers_);
-
-    std::discrete_distribution<> dist_start(start_distribution_.begin(),start_distribution_.end());
+    std::vector<size_t> samp(num_layers());//initialize the state
+    //sample a node of V0 using the starting distribution
+    std::discrete_distribution<> dist_start(
+             start_distribution_.begin(),start_distribution_.end());
     samp[0] = dist_start(gen);
 
     size_t curr_node = samp[0];
-    for(size_t t=0; t<num_layers_-1; ++t) {
+    for(size_t t=0; t<num_layers()-1; ++t) {
+      //folow the transition matrices
       std::discrete_distribution<> dist_t_to_next(
                         decision_mat_[t][curr_node].begin(), 
                         decision_mat_[t][curr_node].end());
@@ -70,29 +104,70 @@ class Markov_decision {
     return samp;
   }
 
-// if alpha > 0, a transition probability p is amplified to become p + |alpha|(1-p). If alpha < 0, p becomes p - |alpha| p
-// -1 < alpha < 1 
+  /** \brief Increase or decrease the probabilities of transitions 
+   * corresponding to a certain state.
+   * 
+   * The input state give the transitions to modify. The float alpha, such that -1< alpha <1 gives the change to apply to the transitions probabilities.
+   * 
+   * If the input state is v0 v1 ... vt, the starting probability to start with v0, and the transition probabilities to go from v0 to v1, to v1 to v2, etc, from vt-1 to vt are modified as follows. Let p be such starting or transition probability:
+   * - if alpha < 0, the probability is decreased to become 
+   * p-|alpha|p,
+   * - if alpha > 0, the probability is increased to become p+|alpha|(1-p). 
+   * 
+   * Each transtition vector is then normalized to add up to 1.
+   * 
+   * @param[in] samp the state for which the transitions are amplified,
+   * @param[in] alpha an float between -1 < alpha < 1.
+   * 
+   * */
   void amplify(std::vector<size_t> samp, Proba_float alpha) {
-    if(alpha > 0) {
-      for(size_t t=0; t<num_layers_-1; ++t) {
-        auto p = decision_mat_[t][ samp[t] ][ samp[t+1] ];//transition proba
-        p += alpha * (1-p);//increase the transition proba
-        decision_mat_[t][ samp[t] ][ samp[t+1] ] = p;//
-        //ensure the entries of the proba row sum to 1
-        normalize_proba(decision_mat_[t][ samp[t] ].begin(), 
-                        decision_mat_[t][ samp[t] ].end() );
+    //increase starting proba
+    auto ps = start_distribution_[samp[0]];
+    if(ps + alpha > 0) { start_distribution_[samp[0]] += alpha; }
+    else { start_distribution_[samp[0]] /= 1.01; } 
+    normalize_proba(start_distribution_.begin(),start_distribution_.end());
+    //increase transition proba
+    for(size_t t=0; t<num_layers()-1; ++t) {
+      auto p = decision_mat_[t][ samp[t] ][ samp[t+1] ];
+      if(p + alpha > 0) { 
+        decision_mat_[t][ samp[t] ][ samp[t+1] ] += alpha;
       }
+      else { decision_mat_[t][ samp[t] ][ samp[t+1] ] /= 1.01; } 
+      //ensure the entries of the proba row sum to 1
+      normalize_proba(decision_mat_[t][ samp[t] ].begin(), 
+                      decision_mat_[t][ samp[t] ].end() );
     }
-    if(alpha < 0 ) {
-      for(size_t t=0; t<num_layers_-1; ++t) {
-        auto p = decision_mat_[t][ samp[t] ][ samp[t+1] ];
-        p += alpha * p;//decrease the proba
-        decision_mat_[t][ samp[t] ][ samp[t+1] ] = p;
-        //ensure the entries of the proba row sum to 1
-        normalize_proba(decision_mat_[t][ samp[t] ].begin(), 
-                        decision_mat_[t][ samp[t] ].end() );
-      }
-    }
+
+    // if(alpha > 0) {
+    //   //increase starting proba
+    //   auto p_start = start_distribution_[samp[0]];
+    //   start_distribution_[samp[0]] += alpha*(1-p_start); 
+    //   normalize_proba(start_distribution_.begin(),start_distribution_.end());
+    //   //increase transition proba
+    //   for(size_t t=0; t<num_layers()-1; ++t) {
+    //     auto p = decision_mat_[t][ samp[t] ][ samp[t+1] ];//transition proba
+    //     p += alpha * (1-p);//increase the transition proba
+    //     decision_mat_[t][ samp[t] ][ samp[t+1] ] = p;//
+    //     //ensure the entries of the proba row sum to 1
+    //     normalize_proba(decision_mat_[t][ samp[t] ].begin(), 
+    //                     decision_mat_[t][ samp[t] ].end() );
+    //   }
+    // }
+    // if(alpha < 0 ) {
+    //   //decrease starting proba
+    //   auto p_start = start_distribution_[samp[0]];
+    //   start_distribution_[samp[0]] += alpha*p_start;
+    //   normalize_proba(start_distribution_.begin(),start_distribution_.end()); 
+    //   //decrease transition proba
+    //   for(size_t t=0; t<num_layers()-1; ++t) {
+    //     auto p = decision_mat_[t][ samp[t] ][ samp[t+1] ];
+    //     p += alpha * p;//decrease the proba
+    //     decision_mat_[t][ samp[t] ][ samp[t+1] ] = p;
+    //     //ensure the entries of the proba row sum to 1
+    //     normalize_proba(decision_mat_[t][ samp[t] ].begin(), 
+    //                     decision_mat_[t][ samp[t] ].end() );
+    //   }
+    // }
   }
 
 //train the decision process
@@ -114,22 +189,83 @@ class Markov_decision {
   //   }
   // }
 
+/** \brief For a given state, try, for each layer, to change a node locally to see if it improves the reward.*/
   template< typename Function >
-  void train(size_t num_iterations, Function &reward) {
+  std::vector<size_t> local_improve(std::vector<size_t> &state, Function &reward) 
+  {
+    auto max_rew = reward(state);
+    std::vector<size_t> imp_state(state);
+    //for all layers
+    for(size_t i = 0; i<imp_state.size(); ++i) {
+      size_t new_s = state[i];
+      //try to change state locally to improve reward
+      for(size_t j=0; j<size_layer(i); ++j) {
+        imp_state[i] = j;
+        auto curr_rew = reward(imp_state);
+        if(curr_rew > max_rew) {
+          max_rew = curr_rew;
+          new_s = j;
+        }
+      }
+      imp_state[i] = new_s;
+    }
+    return imp_state;
+  }
+
+  std::string to_string(std::vector<size_t> &state) {
+    std::string str = "";
+    for(auto i : state) { str = str + std::to_string(i) + " "; }
+    return str;
+  }
+
+  /** \brief Train the Markov decision process by sampling a state, compute its reward and turn it into an amplifying value alpha, and amplify the probabilities of transtion.
+   * 
+   * @param[in] num_iterations the number of runs for the training,
+   * @param[in] reward a reward function to apply on a state ; it returns values in (-1,1) that can be given to the amplify method.
+   **/
+  template< typename Function >
+  std::vector<size_t> train(size_t num_iterations, Function &reward) {
+    
+    Proba_float max_rew = -1;
+    std::vector<size_t> max_state;
+
     for(size_t ite = 0; ite < num_iterations; ++ite) {
       Proba_float rew;
       auto samp = sample();
       rew = reward(samp); //between -1 and 1
+      if(rew > max_rew) { max_rew = rew; max_state = samp; }
       /////
-      std::cout << "Reward = " << rew << "  ---  ";
+      std::cout << "#it: " << ite << " -- Reward = " << rew << "  ---  ";
       for(auto x : samp) { std::cout << x << " "; }
+      std::cout << "    -- Max = " << max_rew << " [";
+      for(auto x : max_state) { std::cout << x << " "; }
+      std::cout << "] ";
       std::cout << std::endl;
+      
+
       /////
+      // Proba_float factor = 10.*(Proba_float)(ite+1)/(Proba_float)num_iterations;
       amplify(samp,rew);
+
+      for(auto p : start_distribution_) { std::cout << p << " "; }
+        std::cout << "\n\n";
     }
+    return max_state;
   }
 
+/** \brief Return the number of layers in the Markov decision process.*/
+  size_t num_layers() { return decision_mat_.size()+1; }
+/** Return the size of a layer given by its index.*/
+  size_t size_layer(size_t i) {
+    if(i<decision_mat_.size()) {
+      if(i==0) { return start_distribution_.size(); }
+      return decision_mat_[i-1].num_columns();
+    }
+    return 0;
+  }
 private:
+  /* Normalize the float numbers in a range such that they sum to 1.
+   */
   template<typename FloatIterator >
   void normalize_proba(FloatIterator beg, FloatIterator end) {
     auto sum = Proba_float(0);
@@ -138,14 +274,32 @@ private:
   }
 
 private:
-  //M[i][j] contains a float that gives the probability of going to state M[i+1][j] when we are in state M[i][j]. The overall decision is the set of col idx visited over time (time = row index)
+  //nodes[l][i] contains the data type of a node in the layer l, l=0...t-1, with label i
+  // std::vector< std::vector<Node_type> > nodes_;
+  //The Markov decision process is encoded as a multipartite graph 
+  //V0 ... Vt with transition |Vi| x |Vi+1| matrices Mi, 
+  //i=0...t-1, for 
+  //transitioning from the nodes of Vi to the nodes of Vi+1
+  /* probability distribution over the nodes of V0 for start*/
   std::vector< Proba_float > start_distribution_;
-  std::vector< Dense_matrix< Proba_float > > decision_mat_;
-  size_t num_steps_;
-
+  //the matrices decision_mat_[i] = Mi
+  std::vector< Dense_matrix< R > > decision_mat_;
+  //the field a real numbers used to work with multiprecision float for the probabilities
   R R_;
 };
 
 } //namespace kumquat
 
 #endif // KUMQUAT_MARKOV_DECISION_H_
+
+
+
+
+
+
+
+
+
+
+
+
