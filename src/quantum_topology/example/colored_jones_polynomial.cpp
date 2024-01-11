1c5b9a786f406b2ee92ff640fc68b6a4690789d3
MARIA Clement <clement.maria@inria.fr>
2024-01-11 16:49:36 +0100

    structure for the colored jones polynomial of a braid, according to Ohtsuki

---
+++ src/quantum_topology/example/colored_jones_polynomial.cpp
@@ -0,0 +1,20 @@
+/*    This file is part of the KUMQUAT Library - https://kumquat.inria.fr/ 
+ *    - released under  GNU GENERAL PUBLIC LICENSE v.3
+ *    See file LICENSE or go to https://kumquat.inria.fr/licensing/ for full 
+ *    license details.
+ *    Author(s):       Clément Maria
+ *
+ *    Copyright (C) 2023 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
+#include <kumquat/xxx.h>
+
+using namespace kumquat;
+
+int main(int argc, char * argv[]) {
+
+  return 0;
+}
