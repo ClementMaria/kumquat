288b72d9a8f043f4d3323f9c502fe444da3c73c8
MARIA Clement <clement.maria@inria.fr>
2024-01-29 08:20:49 +0100

    example quantum group/data

---
+++ src/quantum_topology/example/simple_quantum_data_example.cpp
@@ -0,0 +1,42 @@
+/*    This file is part of the KUMQUAT Library -
+ *    https://kumquat.inria.fr/ 
+ *    - which is a licence protected library. See file LICENSE 
+ *    or go to https://kumquat.inria.fr/licensing/ for full 
+ *    license details.
+ *
+ *    Author(s):       Clément Maria
+ *
+ *    Copyright (C) 2024 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
+#include <iostream>
+#include <kumquat/Quantum_data.h>
+
+using namespace kumquat;
+
+int main() {
+
+	Quantum_data qd;
+	std::cout << "Quantum half X^2-X^{-2} = " << qd.quantum_half() << "\n\n";
+	std::cout << "Quantum monomials X^k, and 1 if k<0: \n";
+	for(int k=-1; k<=10; ++k) {
+		std::cout << "   k = " << k << "  ->  " << qd.quantum_monomial(k) << "\n";
+	}
+	std::cout << "\n";
+	std::cout << "Quantum integers [n] = (X^{2n}-X^{-2n}) / (X^2 - X^{-2}): \n";
+	for(int n=-1; n<=10; ++n) {
+		std::cout << "   n = " << n << "  ->  " << qd.quantum_monomial(n) << "\n";
+	}
+	std::cout << "\n";
+
+	std::cout << "Quantum factorials [n]!: \n";
+	for(int n=-1; n<=10; ++n) {
+		std::cout << "   n = " << n << "  ->  " << qd.quantum_factorial(n) << "\n";
+	}
+	std::cout << "\n";
+
+	return 0;
+}
---
+++ src/quantum_topology/example/simple_quantum_group_example.cpp
@@ -0,0 +1,26 @@
+/*    This file is part of the KUMQUAT Library -
+ *    https://kumquat.inria.fr/ 
+ *    - which is a licence protected library. See file LICENSE 
+ *    or go to https://kumquat.inria.fr/licensing/ for full 
+ *    license details.
+ *
+ *    Author(s):       Clément Maria
+ *
+ *    Copyright (C) 2024 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
+#include <iostream>
+#include <kumquat/Quantum_group.h>
+
+using namespace kumquat;
+
+int main() {
+
+	Quantum_group_Uqsl2_gen_q uq;
+	
+	
+	return 0;
+}
